303c7df0c1101412a7e1fc7e35b1230f24ee716c
Thiago <thirauj@br.ibm.com>
2021-02-13 15:59:58 -0300

    implement UpdateConfigDbStep step

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import json
 from dotenv import load_dotenv
 from server.pipeline.pipeline import Pipeline
 from server.pipeline.steps.initial_step import InitialStep
-from server.pipeline.steps.start_ngrok_step import StartNgrokStep
 from server.pipeline.steps.get_ip_step import GetIPAddressStep
+from server.pipeline.steps.start_ngrok_step import StartNgrokStep
+from server.pipeline.steps.update_config_db_step import UpdateConfigDbStep
 
 load_dotenv()
 
@@ -11,4 +12,5 @@ pipeline = Pipeline()
 pipeline.add_step(InitialStep)
 pipeline.add_step(GetIPAddressStep)
 pipeline.add_step(StartNgrokStep)
+pipeline.add_step(UpdateConfigDbStep)
 pipeline.run()
