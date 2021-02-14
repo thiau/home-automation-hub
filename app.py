1be056d474a8ad0f516b1a26447a6c8ee1fe6eb7
Thiago Araujo <thirauj@br.ibm.com>
2021-02-14 18:28:02 -0300

    Add comments

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import json
 from dotenv import load_dotenv
+
+# Import Pipeline Steps
 from server.pipeline.pipeline import Pipeline
 from server.pipeline.steps.initial_step import InitialStep
 from server.pipeline.steps.get_ip_step import GetIPAddressStep
 from server.pipeline.steps.start_ngrok_step import StartNgrokStep
 from server.pipeline.steps.update_config_db_step import UpdateConfigDbStep
 
+# Load Environment Variables
 load_dotenv()
 
+# Create Pipeline
 pipeline = Pipeline()
 pipeline.add_step(InitialStep)
 pipeline.add_step(GetIPAddressStep)
